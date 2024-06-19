7ae052b114d2957c4e76a39a8c8f71dffcf0e2ba
kaineki <41898575+doanthai@users.noreply.github.com>
2024-06-19 19:26:19 +0700

    Create findDisappearedNumbers.ts

---
+++ leetcode/findDisappearedNumbers.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/
+function findDisappearedNumbers(nums: number[]): number[] {    
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const index = Math.abs(nums[i]) - 1;
+        if (nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            result.push(i + 1);
+        }
+    }
+    return result;
+};
